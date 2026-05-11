f94f91990ba1cae3e358646c5b82407b27bf0ee4
amalps <amalps@rivervalleytechnologies.in>
2026-05-11 11:56:51 +0530

    Attendence & profile page functions

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import multer from 'multer';
 import connectDB from './config/db.js'
 import authRouter from './Routes/authRoutes.js';
 import employeeRouter from './Routes/EmployeeRoutes.js';
+import profileRouter from './Routes/profileRoutes.js';
 
 const app  = express();
 const PORT = process.env.PORT || 4000;
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 });
 app.use('/api/auth', authRouter);
 app.use('/api/employees', employeeRouter);
+app.use('/api/profile', profileRouter);
 
 /** Start Server */
 (async () => {
